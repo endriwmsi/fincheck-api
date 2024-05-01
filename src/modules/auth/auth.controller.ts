d87eb2b0c80d515ad427511ba5d7cb7610616f4e
endriwmsi <endriwmsi@gmail.com>
2024-05-01 16:09:37 -0300

    feat: define public routes and create categories and bankaccouts cruds

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,18 +2,20 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin-dto';
 import { SignUpDto } from './dto/signup-dto';
+import { IsPublic } from 'src/shared/decorators/IsPublic';
 
+@IsPublic()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-in')
-  authenticate(@Body() signInDto: SignInDto) {
+  signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
 
   @Post('sign-up')
-  create(@Body() signIUpDto: SignUpDto) {
+  signUp(@Body() signIUpDto: SignUpDto) {
     return this.authService.signUp(signIUpDto);
   }
 }
